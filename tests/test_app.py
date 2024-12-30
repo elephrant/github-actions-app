f20e04be8f7c92ec2c92833bcfcaa3c20763b5ba
elephrant <slonart@gmail.com>
2024-12-30 18:36:49 +0200

    Add files via upload

---
+++ tests/test_app.py
@@ -0,0 +1,12 @@
+import unittest
+from src.app import app
+
+class TestApp(unittest.TestCase):
+    def test_app(self):
+        self.assertEqual(app.add(1, 2), 3) 
+        self.assertEqual(app.add(1, -2), -1)
+        self.assertEqual(app.add(-1, -2), -3)
+        self.assertEqual(app.add(0, 0), 0)
+
+if __name__ == '__main__':
+    unittest.main()
